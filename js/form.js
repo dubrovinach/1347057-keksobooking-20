b3404b66d47cdfd8b8dc370b619ed9f74a99ab97
dubrovinach <60196551+dubrovinach@users.noreply.github.com>
2020-07-23 22:36:39 +0300

    правки

--- js/form.js
+++ js/form.js
@@ -20,6 +20,8 @@
     checkPriceValidity(evt.target);
   });
 
+  selectTypeOfHousing.addEventListener('change', onTypeChange);
+
   function checkPriceValidity(target) {
     if (target.validity.valueMissing) {
       target.setCustomValidity('Обязательно к заполнению');
@@ -31,6 +33,12 @@
       target.setCustomValidity('');
     }
     target.reportValidity();
+  }
+
+  function onTypeChange(evt) {
+    selectPrice.placeholder = HousePrices[evt.target.value];
+    selectPrice.min = HousePrices[evt.target.value];
+    checkPriceValidity(selectPrice);
   }
 
   var inputTimeIn = document.querySelector('#timein');
@@ -100,6 +108,8 @@
     window.preview.resetPreview();
     evt.preventDefault();
     form.reset();
+    deleteData();
+    window.card.removeCard();
   }
 
   function onFormSubmit(evt) {
@@ -107,6 +117,7 @@
     form.reset();
     deleteData();
     window.preview.resetPreview();
+    window.card.removeCard();
 
     evt.preventDefault();
   }
